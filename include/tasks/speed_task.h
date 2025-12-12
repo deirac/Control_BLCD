833391ba8b94042e9a7cfbe7aa585d2ca5e33938
deirac <ratonmiguelito13@gmail.com>
2025-12-12 07:44:28 -0500

    commit[5]: Update main. implemented final task

--- include/tasks/speed_task.h
+++ include/tasks/speed_task.h
@@ -1,14 +1,6 @@
 #pragma once
 #include <Arduino.h>
 
-/**
- * @brief Inicializa la tarea que convierte target_speed (%)
- *        en tiempo de conmutación (ms) para el motor BLDC.
- */
-void speedControllerTaskInit(uint8_t core);
 
-/**
- * @brief Tarea RTOS que ajusta MotorState.comm_delay_ms
- *        según MotorState.target_speed.
- */
+void speedControllerTaskInit(uint8_t core);
 void speedControllerTask(void* pvParameters);
--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,14 @@
 #include "tasks/pwm_task.h"
 
 void setup() {
-
+  serialTaskInit(0);            // Funciones del puerto serial en core 0.
+  hallTaskInit(0);              // Lectura del sensor Hall en core 0.
+  speedControllerTaskInit(0);   // Control de velocidad (Setpoint).
+  commutationTaskInit(1);       // Conmuta usando las tablas.
+  pwmInit(1);                   // Genera el PWM.
 }
 
 void loop() {
-  // no hace nada 
+  // no hace nada. 
 }
 
